d5ecb460a1896f76ea6eda20bd7b1968add845c2
unknown <ibrahimkhalil.mine@gmail.com>
2022-04-04 23:40:57 +0600

    one chart added

--- src/Component/DashBoard/DashBoard.js
+++ src/Component/DashBoard/DashBoard.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const DashBoard = () => {
+    const [chart, setChart] = useState()
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => setChart(data))
+    }, [])
     return (
-        <div>
-            this is DashBoard 12
+        <div className='align-middle'>
+            <BarChart width={1000} height={550} data={chart}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="sell" fill="#333C83" />
+                <Bar dataKey="revenue" fill="#46244C" />
+            </BarChart>
+            <div>
+
+            </div>
         </div>
     );
 };
