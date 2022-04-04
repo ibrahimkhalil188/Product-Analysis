0d8d59707a0ba2d08e37b9b07e235df333f5e667
unknown <ibrahimkhalil.mine@gmail.com>
2022-04-04 23:06:33 +0600

    review  page done

--- src/Component/HomePage/HomePage.js
+++ src/Component/HomePage/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import headphone from "../../image/headphone.png"
 import useReviews from '../CustomHook/CustomHook';
+import { AiFillStar } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
     const [reviews] = useReviews()
+    const navigate = useNavigate()
     return (
         <div>
             <div className='flex px-12 pt-12 items-center max-w-7xl '>
@@ -21,15 +24,20 @@ const HomePage = () => {
                 <div className='grid grid-cols-3 text-2xl gap-4 bg-slate-900' >
                     {
                         (reviews.slice(0, 3)).map(review =>
-                            <div className='shadow-lg py-20 px-4 rounded-full' key={review.id} >
+                            <div className=' py-20 px-4' key={review.id} >
                                 <p className='bg-slate-200 h-[350px] p-4'>{review.review}</p>
-                                <div className='bg-emerald-500 relative h-[220px]'>
+                                <div className='bg-emerald-500 relative h-[180px]'>
                                     <img className='w-32 h-32 rounded-full absolute top-[-60px] left-[140px] border-4' src={review.image} alt="" />
-                                    <h1 className='absolute top-20 left-[140px]'>{review.name}</h1>
+                                    <div className='absolute top-20 left-[140px]'>
+                                        <h1>{review.name}</h1>
+                                        <p className='flex justify-center items-center'>{review.ratings}<AiFillStar className='text-yellow-600'></AiFillStar></p>
+                                    </div>
                                 </div>
                             </div>
                         )
                     }
+                    <p></p>
+                    <button onClick={() => navigate("/review")} className='text-2xl text-white border-2 rounded-lg py-4 mb-16'>See more reviews</button>
                 </div>
             </div>
         </div>
--- src/Component/Reviews/Reviews.js
+++ src/Component/Reviews/Reviews.js
@@ -1,11 +1,32 @@
 import React from 'react';
-
+import useReviews from '../CustomHook/CustomHook';
+import { AiFillStar } from 'react-icons/ai';
 const Reviews = () => {
+    const [reviews] = useReviews()
     return (
-        <div>
-            <h1>this is review page</h1>
+
+
+        <div className='bg-slate-900'>
+            <h1 className='text-5xl font-mono font-bold py-20 text-emerald-500'>Customer reviews
+            </h1>
+            <div className='grid grid-cols-3 text-2xl gap-4 ' >
+                {
+                    (reviews).map(review =>
+                        <div className='p-6' key={review.id} >
+                            <p className='bg-slate-200 h-[350px] p-4'>{review.review}</p>
+                            <div className='bg-emerald-500 relative h-[180px]'>
+                                <img className='w-32 h-32 rounded-full absolute top-[-60px] left-[140px] border-4' src={review.image} alt="" />
+                                <div className='absolute top-20 left-[140px]'>
+                                    <h1>{review.name}</h1>
+                                    <p className='flex justify-center items-center'>{review.ratings}<AiFillStar className='text-yellow-600'></AiFillStar></p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
         </div>
-    );
+    )
 };
 
 export default Reviews;
