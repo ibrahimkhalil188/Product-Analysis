bdd8c0a5c7986b043a3c6f92261983551780fe1e
unknown <ibrahimkhalil.mine@gmail.com>
2022-04-04 22:37:19 +0600

    review card done

--- src/Component/HomePage/HomePage.js
+++ src/Component/HomePage/HomePage.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import headphone from "../../image/headphone.png"
+import useReviews from '../CustomHook/CustomHook';
 
 const HomePage = () => {
+    const [reviews] = useReviews()
     return (
-        <div className='flex px-12 pt-6 items-center max-w-7xl '>
-            <div className='text-2xl font-mono p-12'>
-                <h1 className='text-5xl text-emerald-500 font-mono font-bold pb-12'>Best Headphone for you</h1>
-                <p>GH32 is one of the best headphone of this year. This headphone is best selling headphone of our website. you can check review of our customer bellow</p>
+        <div>
+            <div className='flex px-12 pt-12 items-center max-w-7xl '>
+                <div className='text-2xl font-mono p-12'>
+                    <h1 className='text-5xl text-emerald-500 font-mono font-bold pb-12'>Best Headphone for you</h1>
+                    <p>GH32 is one of the best headphone of this year. This headphone is best selling headphone of our website. you can check review of our customer bellow</p>
+                </div>
+                <div>
+                    <img className='max-w-2xl h-[75vh]' src={headphone} alt="" />
+                </div>
             </div>
-            <dir>
-                <img className='max-w-2xl h-[75vh]' src={headphone} alt="" />
-            </dir>
+            <div>
+                <h1 className='text-5xl font-mono font-bold py-20 text-emerald-500'>Customer reviews
+                </h1>
+                <div className='grid grid-cols-3 text-2xl gap-4 bg-slate-900' >
+                    {
+                        (reviews.slice(0, 3)).map(review =>
+                            <div className='shadow-lg py-20 px-4 rounded-full' key={review.id} >
+                                <p className='bg-slate-200 h-[350px] p-4'>{review.review}</p>
+                                <div className='bg-emerald-500 relative h-[220px]'>
+                                    <img className='w-32 h-32 rounded-full absolute top-[-60px] left-[140px] border-4' src={review.image} alt="" />
+                                    <h1 className='absolute top-20 left-[140px]'>{review.name}</h1>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 };
